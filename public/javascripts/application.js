29bcf8fd31d29b525c015d0fabebc674306be717
Chris Mear <chris@feedmechocolate.com>
2011-03-06 00:56:01 +0000

    Inline 'insert paragraph' forms.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,16 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+$(document).ready(function () {
+  $('a.insert_paragraph').click(function (event) {
+    event.preventDefault();
+    $(event.target).parent('.control').siblings('.insert_paragraph_form').show('fast');
+    $(event.target).parent('.control').hide();
+  });
+  
+  $('a.cancel').click(function (event) {
+    event.preventDefault();
+    $(event.target).parents('.insert_paragraph_form').hide('fast');
+    $(event.target).parents('.insert_paragraph_form').siblings('.control').show();
+  });
+});
